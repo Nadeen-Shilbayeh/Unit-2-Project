040c0f183f1fb93c5d91a23dfa7b4af66c4ed508
NadeenShilbayeh <shilnade454@gotvdsb.ca>
2020-12-07 13:15:30 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main {
+  //import libraries to to create files
+  import java.io.file;
+  import java.
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
