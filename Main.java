6f094b23220b0a4f1bd7aaf4568180d69010cdc0
NadeenShilbayeh <shilnade454@gotvdsb.ca>
2020-12-07 14:03:20 +0000

    asked them if they wanted to order more

--- Main.java
+++ Main.java
@@ -15,17 +15,20 @@ class Main {
     double [] price = {5.5,6.5,2.5,1.4,1.0};
     int menuOption;
     Scanner input = new Scanner(System.in);
+    String keepUsing;
 
 
     //output screen
-    while (counter <= 5 || keepUsing)
+    while (counter <= 5 || keepUsing.equals("yes"))
     {
-    System.out.println("Choose an item from the menu:");
-    System.out.println("1.Burgers $5.5\n2.Hot Dog $6.5\n3.Fries %2.5\n4.Milk Shake $ 1.4\n5.Water $1.0");
-    menuOption = Integer.parseInt(input.nextLine());
-    System.out.println("How many of them would you like");
-    quantity [menuOption] = Integer.parseInt(input.nextLine());
-
+      System.out.println("Choose an item from the menu:");
+      System.out.println("1.Burgers $5.5\n2.Hot Dog $6.5\n3.Fries %2.5\n4.Milk Shake $ 1.4\n5.Water $1.0");
+      menuOption = Integer.parseInt(input.nextLine());
+      System.out.println("How many of them would you like");
+      quantity[menuOption] = Integer.parseInt(input.nextLine());
+      System.out.println("Would you like to order more?");
+      keepUsing = input.nextLine();
+      counter ++;
     }
   }
 }
