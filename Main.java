5f8fd7f3ca23e1307cefce17748219505eaf2ae2
NadeenShilbayeh <shilnade454@gotvdsb.ca>
2020-12-07 13:54:45 +0000

    asked them what they want and how much of them do they want

--- Main.java
+++ Main.java
@@ -13,16 +13,19 @@ class Main {
     myFile.createNewFile();
     int [] quantity = new int[5];
     double [] price = {5.5,6.5,2.5,1.4,1.0};
+    int menuOption;
     Scanner input = new Scanner(System.in);
 
 
     //output screen
-    System.out.println("Choose an item from the menu:");
-    System.out.println("1.Burgers $5.5\n2.Hot Dog $6.5\n3.Fries\n4.Milk Shake $ 1.4\n5.Water $1.0");
-    
     while (counter <= 5 || keepUsing)
     {
-      fo
+    System.out.println("Choose an item from the menu:");
+    System.out.println("1.Burgers $5.5\n2.Hot Dog $6.5\n3.Fries %2.5\n4.Milk Shake $ 1.4\n5.Water $1.0");
+    menuOption = Integer.parseInt(input.nextLine());
+    System.out.println("How many of them would you like");
+    quantity [menuOption] = Integer.parseInt(input.nextLine());
+
     }
   }
 }
