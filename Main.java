b944db2c55d72fb6d84bf6176dcfdcf1d5bdb490
NadeenShilbayeh <shilnade454@gotvdsb.ca>
2020-12-07 13:21:49 +0000

    added the "throws IOException"

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
-class Main {
-  //import libraries to to create files
-  import java.io.file;
-  import java.io.IOException;
-  //import Filewriter
-  import java.io.FileWriter;
-  //import scanner
+//import libraries to to create files
+import java.io.file;
+import java.io.IOException;
+//import Filewriter
+import java.io.FileWriter;
+//import scanner
+import java.util.Scanner;
 
-  public static void main(String[] args) {
+class Main {
+  public static void main(String[] args) throws IOException {
     System.out.println("Hello world!");
   }
