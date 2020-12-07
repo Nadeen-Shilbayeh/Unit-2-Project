4ba06008aa0c71e44fc6b6bc673a7793dffd424f
NadeenShilbayeh <shilnade454@gotvdsb.ca>
2020-12-07 13:18:57 +0000

    import file writer

--- Main.java
+++ Main.java
@@ -2,6 +2,9 @@ class Main {
   //import libraries to to create files
   import java.io.file;
   import java.io.IOException;
+  //import Filewriter
+  import java.io.FileWriter;
+  //
 
   public static void main(String[] args) {
     System.out.println("Hello world!");
